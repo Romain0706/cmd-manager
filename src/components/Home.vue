900b7188479e5e99794d75495ec2fd5eec50e0cf
Romain Boniface <bonifaceromain59190@gmail.com>
2018-08-14 14:30:10 +0200

    homepage

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
